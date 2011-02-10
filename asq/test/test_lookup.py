69245ef2f0722a0a0e94f548f86f11a3544efd31
Robert Smallshire <robert@smallshire.org.uk>
2011-02-10 15:38:37 +0100

    Additional tests and fixes for Lookup, specifically related to returning an empty sequence when requesting a key which is not present in the Lookup. Tests for Grouping. Tests for join() and the implementation itself. Various minor fixes.

--- asq/test/test_lookup.py
+++ asq/test/test_lookup.py
@@ -1,5 +1,5 @@
 import unittest
-from asq.queryable import Lookup
+from asq.queryable import Lookup, Grouping
 
 __author__ = 'rjs'
 
@@ -74,7 +74,7 @@ class TestLookup(unittest.TestCase):
         self.assertEqual(repr(lookup), "Lookup([('a', 'artichoke'), ('b', 'blackberry'), ('b', 'blueberry'), ('c', 'clementine'), ('c', 'cranberry'), ('c', 'cantaloupe')])")
 
 
-    def test_lookup_getitem(self):
+    def test_lookup_getitem_positive(self):
         k_v = [ ('a', 'artichoke'),
                 ('b', 'blackberry'),
                 ('c', 'clementine'),
@@ -98,6 +98,31 @@ class TestLookup(unittest.TestCase):
         self.assertTrue('cranberry' in g_c)
         self.assertTrue('cantaloupe' in g_c)
 
+    def test_lookup_getitem_negative(self):
+        k_v = [ ('a', 'artichoke'),
+                ('b', 'blackberry'),
+                ('c', 'clementine'),
+                ('b', 'blueberry'),
+                ('c', 'cranberry'),
+                ('c', 'cantaloupe') ]
+
+        lookup = Lookup(k_v)
+        
+        g_d = lookup['d']
+        self.assertEqual(g_d.key, 'd')
+        self.assertEqual(len(g_d), 0)
+
+        g_b = lookup['b']
+        self.assertEqual(g_b.key, 'b')
+        self.assertTrue('blackberry' in g_b)
+        self.assertTrue('blueberry' in g_b)
+
+        g_c = lookup['c']
+        self.assertEqual(g_c.key, 'c')
+        self.assertTrue('clementine' in g_c)
+        self.assertTrue('cranberry' in g_c)
+        self.assertTrue('cantaloupe' in g_c)
+
     def test_lookup_apply_result_selector_default(self):
         k_v = [ ('a', 'artichoke'),
                 ('b', 'blackberry'),
@@ -108,7 +133,10 @@ class TestLookup(unittest.TestCase):
 
         lookup = Lookup(k_v)
         result = lookup.apply_result_selector().to_list()
-        # TODO: How to test?
+        # Rely on the fact that the Lookup is ordered by first key insertion
+        self.assertEqual(result[0], ['artichoke'])
+        self.assertEqual(result[1], ['blackberry', 'blueberry'])
+        self.assertEqual(result[2], ['clementine', 'cranberry', 'cantaloupe'])
 
     def test_lookup_apply_result_selector_default(self):
         k_v = [ ('a', 'artichoke'),
@@ -125,7 +153,43 @@ class TestLookup(unittest.TestCase):
         self.assertTrue('b' in result)
         self.assertTrue('c' in result)
         
+    def test_lookup_is_ordered(self):
+        k_v = [ ('a', 'artichoke'),
+                ('b', 'blackberry'),
+                ('c', 'clementine'),
+                ('b', 'blueberry'),
+                ('c', 'cranberry'),
+                ('c', 'cantaloupe') ]
 
+        lookup = Lookup(k_v)
+        result = lookup.apply_result_selector().to_list()
+        # Rely on the fact that the Lookup is ordered by first key insertion
+        self.assertEqual(list(result[0]), ['artichoke'])
+        self.assertEqual(list(result[1]), ['blackberry', 'blueberry'])
+        self.assertEqual(list(result[2]), ['clementine', 'cranberry', 'cantaloupe'])
 
+    def test_lookup_iterable(self):
+        k_v = [ ('a', 'artichoke'),
+                ('b', 'blackberry'),
+                ('c', 'clementine'),
+                ('b', 'blueberry'),
+                ('c', 'cranberry'),
+                ('c', 'cantaloupe') ]
 
-  
+        lookup = Lookup(k_v)
+        for group in lookup:
+            self.assertTrue(isinstance(group, Grouping))
+
+    def test_lookup_as_queryable(self):
+        k_v = [ ('a', 'artichoke'),
+                ('b', 'blackberry'),
+                ('c', 'clementine'),
+                ('b', 'blueberry'),
+                ('c', 'cranberry'),
+                ('c', 'cantaloupe') ]
+
+        a_group = Lookup(k_v).where(lambda g: g.key == 'a').to_list()
+        self.assertEqual(len(a_group), 1)
+        self.assertTrue(isinstance(a_group[0], Grouping))
+        self.assertEqual(a_group[0].key, 'a')
+        
--- asq/test/test_queryable.py
+++ asq/test/test_queryable.py
@@ -36,9 +36,6 @@ class TestQueryable(unittest.TestCase):
     def test_non_iterable(self):
         self.assertRaises(TypeError, lambda: Queryable(5))
 
-    # TODO: Test: skip_while()
-    # TODO: Test: join()
-
     # TODO: Test each function with an empty sequence
     # TODO: Test each function with an infinite sequence
 
