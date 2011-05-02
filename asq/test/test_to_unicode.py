9e0cf92170272c2b0abbfe5b2abd3960052f6fad
Robert Smallshire <robert@smallshire.org.uk>
2011-05-02 17:22:58 +0200

    Modified to_str to coerce its separator to a str.  Added to_unicode() for Python 2.x. Tests commensurate with API changes.

--- asq/test/test_to_unicode.py
+++ asq/test/test_to_unicode.py
@@ -3,57 +3,57 @@ from asq.queryables import Queryable
 
 __author__ = 'rjs'
 
-class TestToStr(unittest.TestCase):
+class TestToUnicode(unittest.TestCase):
 
-    def test_to_str(self):
+    def test_to_unicode(self):
         a = "This is a string"
-        b = Queryable(a).to_str()
+        b = Queryable(a).to_unicode()
         self.assertEqual(a, b)
 
-    def test_to_str_from_sequence(self):
+    def test_to_unicode_from_sequence(self):
         a = ["This ", "is ", "a ", "string!"]
         b = "This is a string!"
-        c = Queryable(a).to_str()
+        c = Queryable(a).to_unicode()
         self.assertEqual(b, c)
 
-    def test_to_str_from_empty_sequence(self):
+    def test_to_unicode_from_empty_sequence(self):
         a = []
         b = ""
-        c = Queryable(a).to_str()
+        c = Queryable(a).to_unicode()
         self.assertEqual(b, c)
 
-    def test_to_str_empty_with_separator(self):
+    def test_to_unicode_empty_with_separator(self):
         a = []
-        b = Queryable(a).to_str(separator=', ')
+        b = Queryable(a).to_unicode(separator=', ')
         c = ""
         self.assertEqual(b, c)
 
-    def test_to_str_one_with_separator(self):
+    def test_to_unicode_one_with_separator(self):
         a = ['string']
-        b = Queryable(a).to_str(separator=', ')
+        b = Queryable(a).to_unicode(separator=', ')
         c = "string"
         self.assertEqual(b, c)
 
-    def test_to_str_many_with_separator(self):
+    def test_to_unicode_many_with_separator(self):
         a = ['this', 'list', 'will', 'be', 'separated', 'by', 'semicolons']
-        b = Queryable(a).to_str(separator='; ')
+        b = Queryable(a).to_unicode(separator='; ')
         c = "this; list; will; be; separated; by; semicolons"
         self.assertEqual(b, c)
 
-    def test_to_str_stringify_separator(self):
+    def test_to_unicode_stringify_separator(self):
         a = ['this', 'list', 'will', 'be', 'separated', 'by', 'fives']
-        b = Queryable(a).to_str(separator=5)
+        b = Queryable(a).to_unicode(separator=5)
         c = "this5list5will5be5separated5by5fives"
         self.assertEqual(b, c)
 
     def test_stringify_items(self):
         a = [1, 5, 9, 34, 12, 3, 67, 1, 0]
-        b = Queryable(a).to_str(separator=', ')
+        b = Queryable(a).to_unicode(separator=', ')
         c = "1, 5, 9, 34, 12, 3, 67, 1, 0"
         self.assertEqual(b, c)
 
-    def test_to_str_closed(self):
+    def test_to_unicode_closed(self):
         a = ['Aardvark', 'Balloon', 'Carrot', 'Daisy', 'Ecological']
         b = Queryable(a)
         b.close()
-        self.assertRaises(ValueError, lambda: b.to_str())
+        self.assertRaises(ValueError, lambda: b.to_unicode())
