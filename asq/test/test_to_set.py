082ad8747849b928fce1b501fdc2a42acb7fd52d
Robert Smallshire <robert@smallshire.org.uk>
2011-05-02 17:20:16 +0200

    Added some tests for to_set()

--- asq/test/test_to_set.py
+++ asq/test/test_to_set.py
@@ -6,12 +6,6 @@ __author__ = 'rjs'
 class TestToSet(unittest.TestCase):
 
     def test_to_set(self):
-        a = set([1, 2, 4, 8, 16, 32])
-        b = Queryable(a).to_set()
-        c = set([1, 2, 3, 8, 16, 32])
-        self.assertEqual(b, c)
-
-    def test_to_set(self):
         a = [1, 2, 4, 8, 16, 32]
         b = Queryable(a).to_set()
         c = set([1, 2, 4, 8, 16, 32])
