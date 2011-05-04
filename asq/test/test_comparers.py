8a190980fd59fdfcc482376690368f17962ce1e6
Robert Smallshire <robert@smallshire.org.uk>
2011-05-04 08:29:50 +0200

    Added case insensitive comparison documentation and unit tests.

--- asq/test/test_comparers.py
+++ asq/test/test_comparers.py
@@ -1,3 +1,13 @@
+import unittest
+from asq.comparers import insensitive_eq
+
 __author__ = 'rjs'
 
-# TODO: [asq 1.0] Test comparers
+class TestComparers(unittest.TestCase):
+
+    def case_insensitive_positive(self):
+        self.assertTrue(insensitive_eq("abcd", "ABCD"))
+
+    def case_insensitive_negative(self):
+        self.assertFalse(insenstive_eq("abcd", "wxyz"))
+
