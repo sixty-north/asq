728739d4eeb882049f11167d478024a01f96f7ec
Robert Smallshire <robert@smallshire.org.uk>
2011-05-27 09:32:43 +0200

    Renamed a constructor argument of Grouping from group to items. Improved the repr() of Grouping so that it is a valid Python expression which would evaluate to a Grouping of the same value. Added documentation for Grouping.

--- asq/queryables.py
+++ asq/queryables.py
@@ -2548,16 +2548,16 @@ class Grouping(Queryable):
         objects. Instances of this class are retrieved from Lookup objects.
     '''
 
-    def __init__(self, key, group):
+    def __init__(self, key, items):
         '''Create a Grouping with a given key and a collection of members.
 
         Args:
             key: The key corresponding to this Grouping
 
-            group: An iterable collection of the members of the group.
+            items: An iterable collection of the members of the group.
         '''
         self._key = key
-        sequence = list(group)
+        sequence = list(items)
         super(Grouping, self).__init__(sequence)
 
     key = property(lambda self: self._key,
@@ -2592,7 +2592,8 @@ class Grouping(Queryable):
         return self.key != rhs.key or not self.sequence_equal(rhs)
     
     def __repr__(self):
-        return 'Grouping(key={k})'.format(k=repr(self._key))
+        return 'Grouping(key={key}, items={items})'.format(key=repr(self._key),
+                                                     items=repr(self.to_list()))
 
 # TODO: [asq 1.1] Move is_iterable into another file
 
