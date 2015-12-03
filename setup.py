6974e9d1d7ba5560f915db13e45fe2072f3d901c
Robert Smallshire <robert@smallshire.org.uk>
2015-12-03 14:17:52 +0100

    Drop official support for Python 2.6 from asq 1.2.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Environment :: Other Environment",
