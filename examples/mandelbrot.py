4983ad57829426dbf286d1124932d42a114342fe
Robert Smallshire <rob@sixty-north.com>
2024-03-01 13:49:38 +0100

    Updates the mandelbrot.py example

--- examples/mandelbrot.py
+++ examples/mandelbrot.py
@@ -6,9 +6,10 @@ http://msmvps.com/blogs/jon_skeet/archive/2008/02/26/visualising-the-mandelbrot-
 
 '''
 import colorsys
-#import Image
+from PIL import Image
 
-from src.asq.initiators import integers
+from asq import query
+from asq.initiators import integers
 
 
 def generate(start, func):
@@ -29,29 +30,29 @@ def col(n, max):
 
 
 def mandelbrot():
-    MaxIterations = 200
-    SampleWidth = 3.2
-    SampleHeight = 2.5
-    OffsetX = -2.1
-    OffsetY = -1.25
+    max_iterations = 200
+    sample_width = 3.2
+    sample_height = 2.5
+    offset_x = -2.1
+    offset_y = -1.25
 
-    ImageWidth = 480
-    ImageHeight = int(SampleHeight * ImageWidth / SampleWidth)
+    image_width = 480
+    image_height = int(sample_height * image_width / sample_width)
 
-    query = integers(0, ImageHeight).select(lambda y: (y * SampleHeight) / ImageHeight + OffsetY) \
+    q = integers(0, image_height).select(lambda y: (y * sample_height) / image_height + offset_y) \
             .select_many_with_correspondence(
-                lambda y: integers(0, ImageWidth).select(lambda x: (x * SampleWidth) / ImageWidth + OffsetX),
+                lambda y: integers(0, image_width).select(lambda x: (x * sample_width) / image_width + offset_x),
                 lambda y, x: (x, y)) \
             .select(lambda real_imag: complex(*real_imag)) \
             .select(lambda c: query(generate(c, lambda x: x * x + c))
                               .take_while(lambda x: x.real ** 2 + x.imag ** 2 < 4)
-                              .take(MaxIterations)
+                              .take(max_iterations)
                               .count()) \
-            .select(lambda c: ((c * 7) % 255, (c * 5) % 255, (c * 11) % 255) if c != MaxIterations else (0, 0, 0))
+            .select(lambda c: ((c * 7) % 255, (c * 5) % 255, (c * 11) % 255) if c != max_iterations else (0, 0, 0))
 
     data = q.to_list()
 
-    image = Image.new("RGB", (ImageWidth, ImageHeight))
+    image = Image.new("RGB", (image_width, image_height))
     image.putdata(data)
     image.show()
 
